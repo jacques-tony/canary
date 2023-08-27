a04815d0ba0947aa1e0d2ac54acabf63f0be0335
jacques-tony <jakiin.tr@gmail.com>
2023-08-27 14:01:52 -0300

    Update stages.lua

--- data/stages.lua
+++ data/stages.lua
@@ -4,85 +4,121 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7,
-	},
-	{
-		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6,
-	},
-	{
-		minlevel = 21,
-		maxlevel = 50,
-		multiplier = 5,
-	},
-	{
-		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 4,
-	},
-	{
+		multiplier = 900,
+	}, {
 		minlevel = 101,
+		maxlevel = 150,
+		multiplier = 800,
+	}, {
+		minlevel = 151,
+		maxlevel = 200,
+		multiplier = 700,
+	}, { 
+		minlevel = 181,
+		maxlevel = 200,
+		multiplier = 600,
+	}, {
+		minlevel = 201,
+		maxlevel = 230,
+		multiplier = 500,
+	}, {
+		minlevel = 231,
+		maxlevel = 250,
+		multiplier = 400,
+	}, {
+		minlevel = 251,
+		maxlevel = 300,
+		multiplier = 300,
+	}, {
+		minlevel = 301,
+		maxlevel = 400,
+		multiplier = 200,
+	}, {
+		minlevel = 401,
+		maxlevel = 450,
+		multiplier = 100,
+	}, {
+		minlevel = 451,
+		maxlevel = 500,
+		multiplier = 80,
+	}, {
+		minlevel = 501,
+		maxlevel = 550,
+		multiplier = 60,
+	}, {
+		minlevel = 551,
+		maxlevel = 600,
+		multiplier = 40,
+	}, {	
+		minlevel = 601,
+		maxlevel = 700,
+		multiplier = 20,
+		
+	}, {
+		minlevel = 701,
+		maxlevel = 800,
+		multiplier = 10,
+	}, {
+		minlevel = 801,
+		maxlevel = 900,
+		multiplier = 8,
+	}, {	
+		minlevel = 901,
+		maxlevel = 1400,
+		multiplier = 5,
+	}, {
+		minlevel = 1401,
+		maxlevel = 1600,
+		multiplier = 3,
+	}, {
+		minlevel = 1601,
 		multiplier = 2,
-	},
+	}
 }
 
 skillsStages = {
 	{
 		minlevel = 10,
-		maxlevel = 60,
-		multiplier = 15,
-	},
-	{
-		minlevel = 61,
+		maxlevel = 30,
+		multiplier = 50,
+	}, {
+		minlevel = 31,
+		maxlevel = 70,
+		multiplier = 40,
+	}, {
+		minlevel = 71,
 		maxlevel = 80,
-		multiplier = 10,
-	},
-	{
+		multiplier = 20,
+	}, {
 		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6,
-	},
-	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 4,
-	},
-	{
-		minlevel = 126,
-		multiplier = 2,
-	},
+		maxlevel = 95,
+		multiplier = 10,
+	}, {
+		minlevel = 96,
+		multiplier = 5,
+	}
 }
 
 magicLevelStages = {
 	{
 		minlevel = 0,
+		maxlevel = 25,
+		multiplier = 35,
+	}, {
+		minlevel = 26,
+		maxlevel = 40,
+		multiplier = 25,
+	}, {
+		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 10,
-	},
-	{
+		multiplier = 15,
+	}, {
 		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 7,
-	},
-	{
-		minlevel = 81,
-		maxlevel = 100,
+		maxlevel = 90,
 		multiplier = 5,
-	},
-	{
-		minlevel = 101,
-		maxlevel = 110,
-		multiplier = 4,
-	},
-	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 3,
-	},
-	{
-		minlevel = 126,
-		multiplier = 2,
-	},
+	}, {
+		minlevel = 91,
+		multiplier = 5,
+	}
 }
