e5dca2b4c66a70fa8b304b204d4f582d625a1c11
FlackoRJ <phoenixot.suport@gmail.com>
2023-08-30 20:58:13 -0300

    Update
    Verificação de recompensa ilha inicial, e teleport morador no barco...

---
+++ data-otservbr-global/scripts/movements/phoenixMovements/islandNewbie/TownMovement.lua
@@ -0,0 +1,19 @@
+local townMovement = MoveEvent()
+townMovement:type("stepin")
+
+function townMovement.onStepIn(creature, item, position, fromPosition)
+    local player = creature:getPlayer()
+    if not player then
+        return true
+    end
+
+    local town = Town(1)
+    player:setTown(town)
+    player:teleportTo(town:getTemplePosition())
+    town:getTemplePosition():sendMagicEffect(CONST_ME_TELEPORT)
+    player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Agora você é morador da cidade ".. town:getName() .. ".")
+    return true
+end
+
+townMovement:aid(2033)
+townMovement:register()
---
+++ data-otservbr-global/scripts/movements/phoenixMovements/islandNewbie/islandBoat.lua
@@ -0,0 +1,20 @@
+local barcoVerification = MoveEvent()
+
+local Storage = 102030
+
+local quantia = 1
+function barcoVerification.onStepIn(creature, item, position, fromPosition)
+local player = Player(creature)
+if not player then return true end
+
+if player:getStorageValue(Storage) < 1 then
+	player:sendTextMessage(MESSAGE_INFO_DESCR, "Você ainda não pegou seus items.")
+    player:teleportTo({x = 17626, y = 17700, z = 6})		
+	return true
+end
+
+return true
+end
+
+barcoVerification:aid(10995)
+barcoVerification:register() 
