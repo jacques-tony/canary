cdbf62a0f456c96f133f90f1101c23c56e9373dc
jacques-tony <jakiin.tr@gmail.com>
2023-10-07 14:37:49 -0300

    fix: del vocation_reward.lua, actionQuestReset, Add NPC Imbui

--- data-otservbr-global/scripts/actions/phoenixActions/quest/ResetQuest/resetQuest5.lua
+++ data-otservbr-global/scripts/actions/phoenixActions/quest/ResetQuest/resetQuest5.lua
@@ -1,36 +1,36 @@
-	local action_aid, movement_aid, storage = 2256, 2257, 57603
+local action_aid, movement_aid, storage = 2258, 2259, 57604
 
-	local action = Action()
-	function action.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-		if player:getStorageValue(storage) == 1 then
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "Unravel the message of the book.")
+local action = Action()
+function action.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if player:getStorageValue(storage) == 1 then
+	player:sendTextMessage(MESSAGE_INFO_DESCR, "Unravel the message of the book.")
+	return false
+end
+
+	player:setStorageValue(storage, 1)
+	player:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
+	player:sendTextMessage(MESSAGE_INFO_DESCR, "Now you can continue your journey, unravel the message of the book.")
+	return true
+end
+action:aid(action_aid)
+action:register()
+
+local movement = MoveEvent()
+movement:type("stepin")
+function movement.onStepIn(creature, item, position, fromPosition)
+	local player = Player(creature)
+	if not player then
 		return false
 	end
 
-		player:setStorageValue(storage, 1)
-		player:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "Now you can continue your journey, unravel the message of the book.")
+	if player:getStorageValue(storage) == -1 then
+		creature:teleportTo(Position(17607, 17796, 7))
+		player:sendCancelMessage("You can't enter here yet")
 		return true
 	end
-	action:aid(action_aid)
-	action:register()
 
-	local movement = MoveEvent()
-	movement:type("stepin")
-	function movement.onStepIn(creature, item, position, fromPosition)
-		local player = Player(creature)
-		if not player then
-			return false
-		end
-
-		if player:getStorageValue(storage) == -1 then
-			creature:teleportTo(Position(18438, 17356, 15))
-			player:sendCancelMessage("You can't enter here yet")
-			return true
-		end
-
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "Congratulations, continue your journey.")
-		return true
-	end
-	movement:aid(movement_aid)
-	movement:register()
+	player:sendTextMessage(MESSAGE_INFO_DESCR, "Congratulations, continue your journey.")
+	return true
+end
+movement:aid(movement_aid)
+movement:register()
