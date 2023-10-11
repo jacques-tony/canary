bce7ef9b1eb76fb446d719a5475d8ef84ac30e44
FlackoRJ <phoenixot.suport@gmail.com>
2023-10-10 21:18:06 -0300

    FIX: Sistema de reset & tabela de stages

--- data/stages.lua
+++ data/stages.lua
@@ -2,78 +2,106 @@
 -- Maxlevel is OPTIONAL, but is considered infinite by default
 -- Create a stage with minlevel 1 and no maxlevel to disable stages
 experienceStages = {
-	{
+	{ 
 		minlevel = 1,
-		maxlevel = 100,
-		multiplier = 900,
-	}, {
-		minlevel = 101,
-		maxlevel = 150,
-		multiplier = 800,
-	}, {
-		minlevel = 151,
-		maxlevel = 200,
-		multiplier = 700,
-	}, { 
-		minlevel = 181,
-		maxlevel = 200,
-		multiplier = 600,
-	}, {
-		minlevel = 201,
-		maxlevel = 230,
-		multiplier = 500,
-	}, {
-		minlevel = 231,
 		maxlevel = 250,
-		multiplier = 400,
+		multiplier = 1000,
 	}, {
 		minlevel = 251,
 		maxlevel = 300,
-		multiplier = 300,
+		multiplier = 950,
 	}, {
 		minlevel = 301,
+		maxlevel = 350,
+		multiplier = 900,
+	}, { 
+		minlevel = 351,
 		maxlevel = 400,
-		multiplier = 200,
+		multiplier = 850,
 	}, {
 		minlevel = 401,
 		maxlevel = 450,
-		multiplier = 100,
+		multiplier = 800,
 	}, {
 		minlevel = 451,
 		maxlevel = 500,
-		multiplier = 80,
+		multiplier = 750,
 	}, {
 		minlevel = 501,
 		maxlevel = 550,
-		multiplier = 60,
+		multiplier = 700,
 	}, {
 		minlevel = 551,
 		maxlevel = 600,
-		multiplier = 40,
-	}, {	
+		multiplier = 650,
+	}, {
 		minlevel = 601,
+		maxlevel = 650,
+		multiplier = 600,
+	}, {
+		minlevel = 651,
 		maxlevel = 700,
-		multiplier = 20,
-		
+		multiplier = 550,
 	}, {
 		minlevel = 701,
+		maxlevel = 750,
+		multiplier = 500,
+	}, {
+		minlevel = 751,
 		maxlevel = 800,
+		multiplier = 450,
+	}, {	
+		minlevel = 801,
+		maxlevel = 850,
+		multiplier = 400,	
+	}, {
+		minlevel = 851,
+		maxlevel = 900,
+		multiplier = 350,
+	}, {
+		minlevel = 901,
+		maxlevel = 950,
+		multiplier = 300,
+	}, {	
+		minlevel = 951,
+		maxlevel = 1000,
+		multiplier = 250,
+	}, {
+		minlevel = 1001,
+		maxlevel = 1550,
+		multiplier = 200,
+	}, {
+	
+		minlevel = 1551,
+		maxlevel = 2000,
+		multiplier = 100,
+	}, {	
+		minlevel = 2001,
+		maxlevel = 2550,
+		multiplier = 75,
+	}, {
+		minlevel = 2551,
+		maxlevel = 3000,
+		multiplier = 50,
+	}, {
+		minlevel = 3001,
+		maxlevel = 3550,
+		multiplier = 25,
+	}, {
+		minlevel = 3551,
+		maxlevel = 4000,
+		multiplier = 15,
+	}, {	
+		minlevel = 4001,
+		maxlevel = 4550,
 		multiplier = 10,
 	}, {
-		minlevel = 801,
-		maxlevel = 900,
-		multiplier = 8,
-	}, {	
-		minlevel = 901,
-		maxlevel = 1400,
+		minlevel = 4551,
+		maxlevel = 5000,
 		multiplier = 5,
 	}, {
-		minlevel = 1401,
-		maxlevel = 1600,
+		minlevel = 5001,
 		multiplier = 3,
-	}, {
-		minlevel = 1601,
-		multiplier = 2,
 	}
 }
 
