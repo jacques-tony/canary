aa394e4d5428a7b3081cbeb0e72547575f576959
jacques-tony <jakiin.tr@gmail.com>
2024-01-05 12:46:32 -0300

    Update teleport_texts.lua

--- data-otservbr-global/scripts/globalevents/phoenixGlobalevents/teleport_texts.lua
+++ data-otservbr-global/scripts/globalevents/phoenixGlobalevents/teleport_texts.lua
@@ -7,7 +7,14 @@ local effects = {
     {position = Position(17576, 17564, 6), text = 'QUESTS', effect = CONST_ME_GROUNDSHAKER},
     {position = Position(17579, 17564, 6), text = 'TRAINER', effect = CONST_ME_GROUNDSHAKER}, 
     {position = Position(17581, 17564, 6), text = 'BOSSES', effect = CONST_ME_GROUNDSHAKER},
-    {position = Position(17583, 17564, 6), text = 'INVASION', effect = CONST_ME_GROUNDSHAKER},  
+    {position = Position(17583, 17564, 6), text = 'INVASION', effect = CONST_ME_GROUNDSHAKER}, 
+    {position = Position(17569, 17560, 6), text = 'FORGE!', effect = CONST_ME_GROUNDSHAKER}, 
+    {position = Position(17586, 17560, 6), text = 'CASTLE!', effect = CONST_ME_GROUNDSHAKER}, 
+    {position = Position(17564, 17560, 6), text = 'CRAFT!', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17597, 17555, 6), text = 'MINING!', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17574, 17575, 6), text = 'DEPOT!', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17567, 17569, 6), text = 'HUNTED!', effect = CONST_ME_GROUNDSHAKER}, 
+
     ------- RESET
     {position = Position(17503, 17610, 6), text = 'RESET CITY', effect = CONST_ME_GROUNDSHAKER},
     {position = Position(17503, 17611, 6), text = 'VIP SHORTCUT', effect = CONST_ME_GROUNDSHAKER},
@@ -20,15 +27,36 @@ local effects = {
     {position = Position(17510, 17605, 5), text = '+ 9 RESET', effect = CONST_ME_GROUNDSHAKER},
     {position = Position(17510, 17605, 5), text = '+ 2.400 LEVEL', effect = CONST_ME_GROUNDSHAKER},
 
+    ------- MONSTROS
+    {position = Position(17494, 17604, 6), text = '+ 500 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17494, 17604, 6), text = 'MOUNTS ISLAND', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17489, 17604, 6), text = '+ 200 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17489, 17604, 6), text = 'QUIREFANG', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17487, 17608, 6), text = '+ 400 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17487, 17608, 6), text = 'FEYRIST', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17487, 17612, 6), text = '+ 600 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17487, 17612, 6), text = 'INGOL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17487, 17612, 6), text = '+ 800 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17487, 17612, 6), text = 'GNOMPRONA', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17620, 7), text = 'NEED', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17619, 7), text = '+ 200 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+
+    {position = Position(17490, 17618, 8), text = 'NEED', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17618, 8), text = '+ 400 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17623, 8), text = 'NEED', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17623, 8), text = '+ 400 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17621, 9), text = 'NEED', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17620, 9), text = '+ 600 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+
+    {position = Position(17490, 17618, 10), text = 'NEED', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17618, 10), text = '+ 800 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17623, 10), text = 'NEED', effect = CONST_ME_GROUNDSHAKER},
+    {position = Position(17490, 17623, 10), text = '+ 800 LEVEL', effect = CONST_ME_GROUNDSHAKER},
+
     ------- QUESTS
     {position = Position(17548, 17818, 11), text = 'USE!', effect = CONST_ME_GROUNDSHAKER}, 	
     {position = Position(18109, 17700, 12), text = 'USE!', effect = CONST_ME_GROUNDSHAKER}, 	 	
-   -------	
-    {position = Position(17569, 17560, 6), text = 'FORGE!', effect = CONST_ME_GROUNDSHAKER}, 
-    {position = Position(17586, 17560, 6), text = 'CASTLE!', effect = CONST_ME_GROUNDSHAKER}, 
-    {position = Position(17564, 17560, 6), text = 'CRAFT!', effect = CONST_ME_GROUNDSHAKER},
-    {position = Position(17597, 17555, 6), text = 'MINING!', effect = CONST_ME_GROUNDSHAKER},
-    {position = Position(17574, 17575, 6), text = 'DEPOT!', effect = CONST_ME_GROUNDSHAKER},
+
     ------- ILHA INICIAL
     {position = Position(17645, 17694, 5), text = 'Welcome to, PHOENIX OT!', effect = CONST_ME_GROUNDSHAKER},
     {position = Position(17635, 17690, 7), text = 'GO HUNTING!', effect = CONST_ME_GROUNDSHAKER},
