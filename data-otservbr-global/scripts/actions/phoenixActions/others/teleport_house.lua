b80de1838577db4722045f5768cc52a2c1231798
FlackoRJ <141441100+FlackoRJ@users.noreply.github.com>
2024-01-30 20:38:47 -0300

    Update teleport_house.lua

--- data-otservbr-global/scripts/actions/phoenixActions/others/teleport_house.lua
+++ data-otservbr-global/scripts/actions/phoenixActions/others/teleport_house.lua
@@ -1,7 +1,22 @@
 local teleportHouse = Action()
+
 function teleportHouse.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if player:isPzLocked() then
+    -- Verificar se o jogador está em combate
+    if player:getCondition(CONDITION_INFIGHT) then
         player:sendCancelMessage("Não é possível usar enquanto você estiver em combate.")
+        return true
+    end
+
+    -- Verificar se o jogador está PZ Locked
+    if player:isPzLocked() then
+        player:sendCancelMessage("Não é possível usar enquanto você estiver em combate.")
+        return true
+    end
+
+    -- Verificar se o jogador NÃO está em uma zona de proteção ao jogador (PZ zone)
+    local pzInfo = getTilePzInfo(player:getPosition())
+    if not pzInfo then
+        player:sendCancelMessage("Não é possível usar fora de uma zona de proteção ao jogador.")
         return true
     end
  
@@ -10,21 +25,22 @@ function teleportHouse.onUse(player, item, fromPosition, target, toPosition, isH
         player:sendCancelMessage("Você não possui uma casa.")
         return true
     end
-	
-	local storage_time = 235411
-	local timeOnUse  = 2 * 60 -- 2 minutos.
-	
-	if player:getStorageValue(storage_time) > os.time() then
-		local message = "Só pode ser usado novamente ás ".. os.date('%H', player:getStorageValue(storage_time)) ..":".. os.date('%M', player:getStorageValue(storage_time)) ..":".. os.date('%S', player:getStorageValue(storage_time)) .." do dia ".. os.date('%d', player:getStorageValue(storage_time)) .."/".. os.date('%m', player:getStorageValue(storage_time)) .."/".. os.date('%Y', player:getStorageValue(storage_time))
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, message)
-		return true
-	end
+    
+    local storage_time = 235411
+    local timeOnUse  = 2 * 60 -- 2 minutos.
+    
+    if player:getStorageValue(storage_time) > os.time() then
+        local message = "Só pode ser usado novamente ás ".. os.date('%H', player:getStorageValue(storage_time)) ..":".. os.date('%M', player:getStorageValue(storage_time)) ..":".. os.date('%S', player:getStorageValue(storage_time)) .." do dia ".. os.date('%d', player:getStorageValue(storage_time)) .."/".. os.date('%m', player:getStorageValue(storage_time)) .."/".. os.date('%Y', player:getStorageValue(storage_time))
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, message)
+        return true
+    end
 
-	player:say(player:getName() .." foi teleportado para a house.")
-	player:setStorageValue(storage_time, os.time() + timeOnUse)
+    player:say(player:getName() .." foi teleportado para a house.")
+    player:setStorageValue(storage_time, os.time() + timeOnUse)
     player:teleportTo(house:getExitPosition())
     player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-	return true
+    return true
 end
+
 teleportHouse:id(8827) 
 teleportHouse:register()
